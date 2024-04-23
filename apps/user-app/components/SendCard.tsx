323eaa6ddebd1597f9ab8acc36587115543edf2b
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-24 00:58:15 +0530

    refract: p2p server calling

--- apps/user-app/components/SendCard.tsx
+++ apps/user-app/components/SendCard.tsx
@@ -4,11 +4,11 @@ import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/center";
 import { TextInput } from "@repo/ui/textinput";
 import { useState } from "react";
+import { p2pTransfer } from "../lib/actions/p2pTransfer";
+
 
 export function SendCard() {
-    // eslint-disable-next-line no-unused-vars
     const [number, setNumber] = useState("");
-    // eslint-disable-next-line no-unused-vars
     const [amount, setAmount] = useState("");
 
     return <div className="h-[90vh]">
@@ -22,8 +22,8 @@ export function SendCard() {
                         setAmount(value)
                     }} />
                     <div className="pt-4 flex justify-center">
-                        <Button onClick={() => {
-
+                        <Button onClick={async () => {
+                            await p2pTransfer(number, Number(amount) * 100)
                         }}>Send</Button>
                     </div>
                 </div>
